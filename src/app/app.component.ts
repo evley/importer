762092223840c690f8b2c8fa3f4aeb8f08ebb130
tinybigideas <craig@tinybigideas.com>
2020-02-11 13:01:22 +0000

    Initial GSHEET service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { ImportDialogData } from './import-dialog/import-dialog-data.interface';
+import { ImportDialogComponent } from './import-dialog/import-dialog.component';
+import { ImportMethod } from './import-dialog/method/import-method.enum';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'importer';
+  constructor(private _dialog: MatDialog) {}
+
+  public importData(): void {
+    const data: ImportDialogData = {
+      appId: 'fire',
+      methods: [ImportMethod.CSV, ImportMethod.GSHEET]
+    };
+    const dialogRef = this._dialog.open(ImportDialogComponent, {
+      maxWidth: '50vw',
+      autoFocus: false,
+      data
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
 }
